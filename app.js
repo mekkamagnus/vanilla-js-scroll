5572db889a348d16f2bbf1bf841699ee1bc680c3
Mekael Turner <mistnpeace@gmail.com>
2021-11-17 15:57:20 +0800

    Change menu height to adjust dynamicaly

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
-  // show-links has a hard coded height for the menu
-  linksContainer.classList.toggle("show-links");
+  // menu height set dynamically
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
 });
 
 // ********** fixed navbar ************
